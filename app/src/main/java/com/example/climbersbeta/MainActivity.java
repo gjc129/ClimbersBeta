aea6ec9525186df4f1caa9a0e275e275d3143942
gjc129 <cabrera.gabriel94@me.com>
2019-03-09 13:19:34 -0800

    Added the viewbyid functions for all views

--- app/src/main/java/com/example/climbersbeta/MainActivity.java
+++ app/src/main/java/com/example/climbersbeta/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.climbersbeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    //View declarations
+    TextView tvRouteTitle;
+    TextView tvRating;
+    TextView tvDescription;
+    TextView tvDescriptionBody;
+    TextView tvLocation;
+    TextView tvLocationBody;
+    TextView tvProtection;
+    TextView tvProtectionBody;
+    TextView tvType;
+    TextView tvTypeBody;
+    ImageView ivRouteImage;
+    RatingBar rbRating;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find id for all views
+        tvRouteTitle = findViewById(R.id.tvRouteTitle);
+        tvRating = findViewById(R.id.tvRating);
+        tvDescription = findViewById(R.id.tvDescription);
+        tvDescriptionBody = findViewById(R.id.tvDescriptionBody);
+        tvLocation = findViewById(R.id.tvLocation);
+        tvLocationBody = findViewById(R.id.tvLocationBody);
+        tvProtection = findViewById(R.id.tvProtection);
+        tvProtectionBody = findViewById(R.id.tvProtectionBody);
+        tvType = findViewById(R.id.tvType);
+        tvTypeBody = findViewById(R.id.tvTypeBody);
+        ivRouteImage = findViewById(R.id.ivRouteImage);
+        rbRating = findViewById(R.id.rbRating);
+
+        ivRouteImage.setImageResource(R.drawable.tinman);
+        rbRating.setRating((float) 2.3);
+
+
+
     }
 }
