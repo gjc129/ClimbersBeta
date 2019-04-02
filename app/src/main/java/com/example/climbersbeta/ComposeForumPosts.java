03ace2e4fd3ee79641289b492511df9f1b630d12
mar807 <mar807@humboldt.edu>
2019-04-02 14:10:53 -0700

    Added a compose forum post page and added the viewbyid's

---
+++ app/src/main/java/com/example/climbersbeta/ComposeForumPosts.java
@@ -0,0 +1,26 @@
+package com.example.climbersbeta;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ComposeForumPosts extends AppCompatActivity {
+
+    // View declarations
+    EditText etForumPost;
+    Button btnBack;
+    Button btnPost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.compose_forum_post);
+
+        // Find id for all views
+        etForumPost = findViewById(R.id.etForumPost);
+        btnBack = findViewById(R.id.btnBack);
+        btnPost = findViewById(R.id.btnPost);
+    }
+}
