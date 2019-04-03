ebd7b41c8a9a9be2a6b3078a93b61a0968d3811c
gjc129 <cabrera.gabriel94@me.com>
2019-04-03 13:29:21 -0700

    Renamed pages to align with writing code standards

---
+++ app/src/main/java/com/example/climbersbeta/MainLayoutPage.java
@@ -0,0 +1,33 @@
+package com.example.climbersbeta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainLayoutPage extends AppCompatActivity
+{
+
+    //declaration of views
+    ImageView ivMap;
+    Button btnBrowse;
+    Button btnCreateAccount;
+    Button btnForum;
+    Button btnBulletinBoard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout_page);
+
+        //find ID for all values
+        ivMap = findViewById(R.id.ivMap);
+        btnBrowse = findViewById(R.id.btnBrowse);
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+        btnForum = findViewById(R.id.btnForum);
+        btnBulletinBoard = findViewById(R.id.btnBulletinBoard);
+
+
+    }
+}
