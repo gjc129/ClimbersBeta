52e6744fdc3f27b9fa2799ff69db277047909109
gjc129 <cabrera.gabriel94@me.com>
2019-04-08 22:40:46 -0700

    Created BulletinPostPage Created MapsActivityPage Created xml files for each Added onclicklisteners for: Map Forum Bulletin Browse

--- app/src/main/java/com/example/climbersbeta/MainForumPage.java
+++ app/src/main/java/com/example/climbersbeta/MainForumPage.java
@@ -1,8 +1,11 @@
 package com.example.climbersbeta;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 
 public class MainForumPage extends AppCompatActivity
@@ -11,15 +14,26 @@ public class MainForumPage extends AppCompatActivity
     // View declarations
     Button btnComposePost;
     RecyclerView rvPosts;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.forum_main_page);
 
         // Find view by id's
         btnComposePost = findViewById(R.id.btnComposePost);
         rvPosts = findViewById(R.id.rvPosts);
+
+        rvPosts.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent composeForumPost = new Intent(context, ComposeForumPosts.class);
+                context.startActivity(composeForumPost);
+            }
+        });
     }
 }
