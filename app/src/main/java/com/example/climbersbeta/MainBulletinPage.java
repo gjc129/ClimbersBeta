3204d5a3a9c180704fbd315ee13e69b80b3f78ff
mar807 <mar807@humboldt.edu>
2019-04-10 14:26:54 -0700

    Recommiting to see if it works now.

--- app/src/main/java/com/example/climbersbeta/MainBulletinPage.java
+++ app/src/main/java/com/example/climbersbeta/MainBulletinPage.java
@@ -1,13 +1,32 @@
 package com.example.climbersbeta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainBulletinPage extends AppCompatActivity {
+
+    // declaration of views
+    Button btnComposePost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_bulletin_page);
+
+        btnComposePost = findViewById(R.id.btnComposePost);
+
+
+        // the compose button will send you to the Bulletin Post Page
+        btnComposePost.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent sendToBullPost = new Intent(MainBulletinPage.this, BulletinPostPage.class);
+                startActivity(sendToBullPost);
+            }
+        });
     }
 }
+
--- app/src/main/java/com/example/climbersbeta/MainForumPage.java
+++ app/src/main/java/com/example/climbersbeta/MainForumPage.java
@@ -1,8 +1,10 @@
 package com.example.climbersbeta;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 
 public class MainForumPage extends AppCompatActivity
@@ -16,10 +18,21 @@ public class MainForumPage extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_forum_page);
 
         // Find view by id's
         btnComposePost = findViewById(R.id.btnComposePost);
         rvPosts = findViewById(R.id.rvPosts);
+
+        // clicking on compose post will take you to compose forum post page
+        btnComposePost.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent sendToForum = new Intent(MainForumPage.this, ComposeForumPosts.class);
+                startActivity(sendToForum);
+            }
+        });
     }
+
 }
