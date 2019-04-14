b04411e9981fc1ba04d898e5934b86e53db94ab1
gjc129 <cabrera.gabriel94@me.com>
2019-04-14 16:37:17 -0700

    Fixed issues with forum page not going to next page Fixed issues with bulletin page not going to next page Removed Action Bar from main page(for aestetics) Added a textview for the title of the app on the main page, will be replacing later with much better looking image of text. need to work with photoshop on the design for the buttons, texts etc.

--- app/src/main/java/com/example/climbersbeta/MainBulletinPage.java
+++ app/src/main/java/com/example/climbersbeta/MainBulletinPage.java
@@ -1,13 +1,32 @@
 package com.example.climbersbeta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainBulletinPage extends AppCompatActivity {
+
+    Button btnComposePost;
+    RecyclerView rvBulletinPosts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_bulletin_page);
+
+        btnComposePost = findViewById(R.id.btnComposeBulletinPost);
+        rvBulletinPosts = findViewById(R.id.rvBulletinPosts);
+
+        btnComposePost.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent sendToBulletinPostPage = new Intent(MainBulletinPage.this, BulletinPostPage.class);
+                startActivity(sendToBulletinPostPage);
+            }
+        });
+
     }
 }
